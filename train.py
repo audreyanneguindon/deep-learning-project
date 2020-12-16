536f425c1c2e1815a06549fe5abd7af2783541ad
Audrey-Anne Guindon <audreyanne.guindon@shopify.com>
2020-12-16 16:19:23 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,281 +1,64 @@
-import torch
-import torch.nn as nn
-from torch import optim
-import torch.backends.cudnn as cudnn
+import argparse
+from train import trainIters
+from evaluate import runTest
 
-import itertools
-import random
-import math
-import os
-from tqdm import tqdm
-from load import loadPrepareData
-from load import SOS_token, EOS_token, PAD_token
-from model import *
-from config import MAX_LENGTH, teacher_forcing_ratio, save_dir
+def parse():
+    parser = argparse.ArgumentParser(description='Attention Seq2Seq Chatbot')
+    parser.add_argument('-tr', '--train', help='Train the model with corpus')
+    parser.add_argument('-te', '--test', help='Test the saved model')
+    parser.add_argument('-l', '--load', help='Load the model and train')
+    parser.add_argument('-c', '--corpus', help='Test the saved model with vocabulary of the corpus')
+    parser.add_argument('-a', '--attn', help='Attention model for decoder')
+    parser.add_argument('-r', '--reverse', action='store_true', help='Reverse the input sequence')
+    parser.add_argument('-f', '--filter', action='store_true', help='Filter to small training data set')
+    parser.add_argument('-i', '--input', action='store_true', help='Test the model by input the sentence')
+    parser.add_argument('-it', '--iteration', type=int, default=10000, help='Train the model with it iterations')
+    parser.add_argument('-pi', '--prints', type=int, default=100, help='Print every i iterations')
+    parser.add_argument('-b', '--batch_size', type=int, default=64, help='Batch size')
+    parser.add_argument('-la', '--layer', type=int, default=1, help='Number of layers in encoder and decoder')
+    parser.add_argument('-hi', '--hidden', type=int, default=256, help='Hidden size in encoder and decoder')
+    parser.add_argument('-be', '--beam', type=int, default=1, help='Beam size')
+    parser.add_argument('-k', '--topk', type=int, default=1, help='Top-K size')
+    parser.add_argument('-p', '--nucleus', type=float, default=0.0, help='Nucleus (top-p) size')
+    parser.add_argument('-v', '--ved', type=int, default=0, help='Variational encoder-decoder model')
+    parser.add_argument('-s', '--save', type=int, default=500, help='Save every s iterations')
+    parser.add_argument('-lr', '--learning_rate', type=float, default=0.01, help='Learning rate')
+    parser.add_argument('-cl', '--clip', type=float, default=50.0, help='Clip rate')
+    parser.add_argument('-d', '--dropout', type=float, default=0.1, help='Dropout probability for rnn and dropout layers')
 
-USE_CUDA = torch.cuda.is_available()
-device = torch.device("cuda" if USE_CUDA else "cpu")
+    args = parser.parse_args()
+    return args
 
-cudnn.benchmark = True
-#############################################
-# generate file name for saving parameters
-#############################################
-def filename(reverse, obj):
-	filename = ''
-	if reverse:
-		filename += 'reverse_'
-	filename += obj
-	return filename
+def parseFilename(filename, test=False):
+    filename = filename.split('/')
+    dataType = filename[-1][:-4] # remove '.tar'
+    parse = dataType.split('_')
+    reverse = 'reverse' in parse
+    layers, hidden = filename[-2].split('_')
+    n_layers = int(layers.split('-')[0])
+    hidden_size = int(hidden)
+    return n_layers, hidden_size, reverse
 
+def run(args):
+    reverse, fil, n_iteration, print_every, save_every, learning_rate, \
+        n_layers, hidden_size, batch_size, attn_model, beam_size, k, p, v, inp, clip, dropout = \
+        args.reverse, args.filter, args.iteration, args.prints, args.save, args.learning_rate, \
+        args.layer, args.hidden, args.batch_size, args.attn, args.beam, args.topk, args.nucleus, args.ved, args.input, \
+        args.clip, args.dropout
 
-#############################################
-# Prepare Training Data
-#############################################
-def indexesFromSentence(voc, sentence):
-    return [voc.word2index[word] for word in sentence.split(' ')] + [EOS_token]
+    if args.train and not args.load:
+        trainIters(args.train, reverse, n_iteration, learning_rate, batch_size, v,
+                    n_layers, hidden_size, print_every, save_every, clip, dropout)
 
-# batch_first: true -> false, i.e. shape: seq_len * batch
-def zeroPadding(l, fillvalue=PAD_token):
-    return list(itertools.zip_longest(*l, fillvalue=fillvalue))
-
-def binaryMatrix(l, value=PAD_token):
-    m = []
-    for i, seq in enumerate(l):
-        m.append([])
-        for token in seq:
-            if token == PAD_token:
-                m[i].append(0)
-            else:
-                m[i].append(1)
-    return m
-
-# convert to index, add EOS
-# return input pack_padded_sequence
-def inputVar(l, voc):
-    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
-    lengths = [len(indexes) for indexes in indexes_batch]
-    padList = zeroPadding(indexes_batch)
-    padVar = torch.LongTensor(padList)
-    return padVar, lengths
-
-# convert to index, add EOS, zero padding
-# return output variable, mask, max length of the sentences in batch
-def outputVar(l, voc):
-    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
-    max_target_len = max([len(indexes) for indexes in indexes_batch])
-    padList = zeroPadding(indexes_batch)
-    mask = binaryMatrix(padList)
-    mask = torch.ByteTensor(mask)
-    padVar = torch.LongTensor(padList)
-    return padVar, mask, max_target_len
-
-# pair_batch is a list of (input, output) with length batch_size
-# sort list of (input, output) pairs by input length, reverse input
-# return input, lengths for pack_padded_sequence, output_variable, mask
-def batch2TrainData(voc, pair_batch, reverse):
-    if reverse:
-        pair_batch = [pair[::-1] for pair in pair_batch]
-    pair_batch.sort(key=lambda x: len(x[0].split(" ")), reverse=True)
-    input_batch, output_batch = [], []
-    for pair in pair_batch:
-        input_batch.append(pair[0])
-        output_batch.append(pair[1])
-    inp, lengths = inputVar(input_batch, voc)
-    output, mask, max_target_len = outputVar(output_batch, voc)
-    return inp, lengths, output, mask, max_target_len
-
-#############################################
-# Define Loss Function
-#############################################
-def maskNLLLoss(inp, target, mask):
-    nTotal = mask.sum()
-    crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1, 1)))
-    loss = crossEntropy.masked_select(mask).mean()
-    loss = loss.to(device)
-    return loss, nTotal.item()
-
-#############################################
-# Training
-#############################################
-
-def train(input_variable, lengths, target_variable, target_lengths, mask, max_target_len, encoder, hidvar, decoder,
-          embedding, encoder_optimizer, decoder_optimizer, batch_size, clip, max_length=MAX_LENGTH):
-
-    # Zero gradients
-    encoder_optimizer.zero_grad()
-    decoder_optimizer.zero_grad()
-    if hidvar:
-        hidvar_optimizer.zero_grad()
-
-    # Set device options
-    input_variable = input_variable.to(device)
-    target_variable = target_variable.to(device)
-    mask = mask.to(device)
-
-    # Initialize variables
-    loss = 0
-    print_losses = []
-    n_totals = 0
-
-    encoder_outputs, encoder_hidden = encoder(input_variable, lengths, None)
-    _, response_encoder_hidden = encoder(target_variable, target_lengths)
-
-    if hidvar:
-        z, mean, logvar = hidvar(encoder_hidden, response_encoder_hidden)
-        kl_loss = (-0.5 * torch.sum(logvar - torch.pow(mean, 2) - torch.exp(logvar) + 1, 1)).mean().squeeze()
-
-        decoder_input = torch.LongTensor([[SOS_token for _ in range(batch_size)]])
-        decoder_input = decoder_input.to(device)
-
-        decoder_hidden = torch.cat([encoder_hidden[:decoder.n_layers], z[:decoder.n_layers]], 2)
-    else:
-        kl_loss = 1
-        decoder_input = torch.LongTensor([[SOS_token for _ in range(batch_size)]])
-        decoder_input = decoder_input.to(device)
-
-        decoder_hidden = encoder_hidden[:decoder.n_layers]
-
-    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
-
-    # Run through decoder one time step at a time
-    if use_teacher_forcing:
-        for t in range(max_target_len):
-            decoder_output, decoder_hidden, _ = decoder(
-                decoder_input, decoder_hidden, encoder_outputs
-            )
-            decoder_input = target_variable[t].view(1, -1) # Next input is current target
-            mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[t], mask[t]) # import maskNLLLoss
-            loss += mask_loss + kl_loss
-            print_losses.append(mask_loss.item() * nTotal)
-            n_totals += nTotal
-    else:
-        for t in range(max_target_len):
-            decoder_output, decoder_hidden, decoder_attn = decoder(
-                decoder_input, decoder_hidden, encoder_outputs
-            )
-            _, topi = decoder_output.topk(1) # [64, 1]
-
-            decoder_input = torch.LongTensor([[topi[i][0] for i in range(batch_size)]])
-            decoder_input = decoder_input.to(device)
-            mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[t], mask[t])
-            loss += mask_loss + kl_loss
-            print_losses.append(mask_loss.item() * nTotal)
-            n_totals += nTotal
-
-    loss.backward()
-
-    clip = 50.0
-    _ = torch.nn.utils.clip_grad_norm_(encoder.parameters(), clip)
-    _ = torch.nn.utils.clip_grad_norm_(decoder.parameters(), clip)
-
-    encoder_optimizer.step()
-    decoder_optimizer.step()
-    if hidvar:
-        hidvar_optimizer.step()
-
-    return sum(print_losses) / n_totals
+    elif args.load:
+        n_layers, hidden_size, reverse = parseFilename(args.load)
+        trainIters(args.train, reverse, n_iteration, learning_rate, batch_size, v,
+                    n_layers, hidden_size, print_every, save_every, dropout, loadFilename=args.load)
+    elif args.test:
+        n_layers, hidden_size, reverse = parseFilename(args.test, True)
+        runTest(n_layers, hidden_size, reverse, args.test, attn_model, beam_size, k, p, v, inp, args.corpus)
 
 
-def trainIters(corpus, reverse, n_iteration, learning_rate, batch_size, v, n_layers, hidden_size, print_every,
-               save_every, clip, dropout, loadFilename=None, attn_model='dot', decoder_learning_ratio=5.0):
-
-    voc, pairs = loadPrepareData(corpus)
-
-    # training data
-    corpus_name = os.path.split(corpus)[-1].split('.')[0]
-    training_batches = None
-    try:
-        training_batches = torch.load(os.path.join(save_dir, 'training_data', corpus_name,
-                                                   '{}_{}_{}.tar'.format(n_iteration, \
-                                                                         filename(reverse, 'training_batches'), \
-                                                                         batch_size)))
-    except FileNotFoundError:
-        print('Training pairs not found, generating ...')
-        training_batches = [batch2TrainData(voc, [random.choice(pairs) for _ in range(batch_size)], reverse)
-                          for _ in range(n_iteration)]
-        torch.save(training_batches, os.path.join(save_dir, 'training_data', corpus_name,
-                                                  '{}_{}_{}.tar'.format(n_iteration, \
-                                                                        filename(reverse, 'training_batches'), \
-                                                                        batch_size)))
-    # model
-    checkpoint = None
-    print('Building encoder and decoder ...')
-
-    embedding = nn.Embedding(voc.num_words, hidden_size)
-
-    if v:
-        embedding_decoder = nn.Embedding(voc.num_words, hidden_size * 2)
-        encoder = EncoderRNN(hidden_size, embedding, n_layers, dropout)
-        decoder = DecoderRNN(attn_model, embedding_decoder, hidden_size * 2, voc.num_words, n_layers, dropout)
-        hidvar = LatentVariation(2 * hidden_size, hidden_size)
-    else:
-        encoder = EncoderRNN(voc.num_words, hidden_size, embedding, n_layers, dropout)
-        attn_model = attn_model
-        decoder = LuongAttnDecoderRNN(attn_model, embedding, hidden_size, voc.num_words, n_layers, dropout)
-        hidvar = 0
-
-    if loadFilename:
-        checkpoint = torch.load(loadFilename)
-        encoder.load_state_dict(checkpoint['en'])
-        decoder.load_state_dict(checkpoint['de'])
-        if v:
-            hidvar.load_state_dict(checkpoint['hv'])
-    # use cuda
-    encoder = encoder.to(device)
-    decoder = decoder.to(device)
-    if v:
-        hidvar = hidvar.to(device)
-
-    # optimizer
-    print('Building optimizers ...')
-    encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
-    decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate * decoder_learning_ratio)
-    if v:
-        hidvar_optimizer = optim.Adam(hidvar.parameters(), lr=learning_rate)
-
-    if loadFilename:
-        encoder_optimizer.load_state_dict(checkpoint['en_opt'])
-        decoder_optimizer.load_state_dict(checkpoint['de_opt'])
-        if v:
-            hidvar_optimizer.load_state_dict(checkpoint['hv_opt'])
-
-    # initialize
-    print('Initializing ...')
-    start_iteration = 1
-    perplexity = []
-    print_loss = 0
-    if loadFilename:
-        start_iteration = checkpoint['iteration'] + 1
-        perplexity = checkpoint['plt']
-
-    # training loop
-    print('Training ...')
-    for iteration in tqdm(range(start_iteration, n_iteration + 1)):
-        training_batch = training_batches[iteration - 1]
-        input_variable, lengths, target_variable, mask, max_target_len = training_batch
-
-        loss = train(input_variable, lengths, target_variable, target_lengths, mask, max_target_len, encoder,
-                     decoder, hidvar, embedding, encoder_optimizer, decoder_optimizer, batch_size)
-        print_loss += loss
-        perplexity.append(loss)
-
-        if iteration % print_every == 0:
-            print_loss_avg = math.exp(print_loss / print_every)
-            print('%d %d%% %.4f' % (iteration, iteration / n_iteration * 100, print_loss_avg))
-            print_loss = 0
-
-        if (iteration % save_every == 0):
-            directory = os.path.join(save_dir, 'model', corpus_name, '{}-{}_{}'.format(n_layers, n_layers, hidden_size))
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            torch.save({
-                'iteration': iteration,
-                'en': encoder.state_dict(),
-                'de': decoder.state_dict(),
-                'hv': hidvar.state_dict(),
-                'en_opt': encoder_optimizer.state_dict(),
-                'de_opt': decoder_optimizer.state_dict(),
-                'hv_opt': hidvar_optimizer.state_dict(),
-                'loss': loss,
-                'plt': perplexity
-            }, os.path.join(directory, '{}_{}.tar'.format(iteration, filename(reverse, 'backup_bidir_model'))))
+if __name__ == '__main__':
+    args = parse()
+    run(args)
